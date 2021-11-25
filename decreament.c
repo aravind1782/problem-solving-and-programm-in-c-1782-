7d251966d79f8f2155707238729749e82f83fb69
aravind1782 <93912702+aravind1782@users.noreply.github.com>
2021-11-25 10:00:23 +0530

    Create decreament.c

---
+++ decreament.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a=1;int b=2;
+    for(a=1;a<20;a++)
+    {
+    	printf("\na after increament %d",a);
+	}
+	for(b=2;b<50;b++)
+	printf("\nb after decreament %d",b);
+    
+    
+    return 0;
+}
