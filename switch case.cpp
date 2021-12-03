cfb45ad569848847f8c10508a7282887a02b4e28
aravind1782 <93912702+aravind1782@users.noreply.github.com>
2021-12-03 09:04:12 +0530

    Add files via upload

--- switch case.cpp
+++ switch case.cpp
@@ -1,45 +1,31 @@
 #include<stdio.h>
-int main()
+main()
 {
-	int k,h,m,s,km,mt,cm,ch,d,c,f,cbm,ml,l;
-	printf("enter the choice");
-	scanf("%d",&ch);
-	switch(ch)
-	{
-	case 1:
-	printf("enter the value h");
-	m=h*60;
-	s=m*60;
-	printf(" %d",m);
-	printf(" \n%d",s);
-	break;
-    case 2:
-	printf("\nenter the value km ");
-	scanf("%d",&km);
-	mt=km*1000;
-	cm=mt*100;
-	printf(" \n %d ",mt);
-	printf(" \n %d ",cm);
-	break;
-	case 3:
-	printf("\nenter celcius value:");
-	scanf("%d",&c);
-	f=(c*9/5)+32;
-	k=c+273;
-	printf("\n%d",f);
-	printf("\n%d",k);
-	break;
-	case 4:
-	printf("\nEnter the litre");
-	scanf("%d",&l);
-	ml=l*1000;
-	cbm=ml*1000;
-	printf("\n%d",ml);
-    printf("\n%d",cbm);
-	break;
-	}
-
-	return 0;
-	
-	
+   int a,b,c,d,e,f,ch;
+   float g,h;
+   printf("enter the value a");
+   scanf ("%d",&a);
+   printf("enter the value b");
+   scanf("%d",&b);
+   printf("enter your choice");
+   scanf("%d",&ch);
+   switch(ch)
+   {
+   case 1:
+   c=a+b;
+  printf("%d",c); 
+   break;
+   case 2:
+   d=a-b;
+   printf("%d",d);
+   break;
+   case 3:
+   e=a*b;
+   printf("%d",e);
+   break;
+   case 4:
+   f=a/b;
+   printf("%d",f);
+   break;
+  }
 }
